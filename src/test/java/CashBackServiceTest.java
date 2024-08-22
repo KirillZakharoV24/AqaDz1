4852ca42a8b10a618ed19d3f9bd96e449f96e713
Kirill Zakharov <kirilll2491@gmail.com>
2024-08-22 10:39:56 +0500

    Add testng

--- src/test/java/CashBackServiceTest.java
+++ src/test/java/CashBackServiceTest.java
@@ -1,55 +1,55 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
 import ru.topacademy.CashBackService;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class CashBackServiceTest {
-    @Test
-    void testServiceInBoundary() {
+    @org.testng.annotations.Test
+    public void testServiceInBoundary() {
         CashBackService service = new CashBackService();
 
         int expected = 0;
         int actual = service.remain(1000);
 
-        Assertions.assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void testServiceUnderBoundary() {
+    @org.testng.annotations.Test
+    public void testServiceUnderBoundary() {
         CashBackService service = new CashBackService();
 
         int expected = 10;
         int actual = service.remain(990);
 
-        Assertions.assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void testServiceBelowBoundary() {
+    @org.testng.annotations.Test
+    public void testServiceBelowBoundary() {
         CashBackService service = new CashBackService();
 
         int expected = 990;
         int actual = service.remain(1010);
-
-        Assertions.assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void testServiceBorderBoundary() {
+    @org.testng.annotations.Test
+    public void testServiceBorderBoundary() {
         CashBackService service = new CashBackService();
 
         int expected = 1;
         int actual = service.remain(999);
 
-        Assertions.assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void testServiceBorderBoundary1() {
+    @org.testng.annotations.Test
+    public void testServiceBorderBoundary1() {
         CashBackService service = new CashBackService();
 
         int expected = 999;
         int actual = service.remain(1001);
 
-        Assertions.assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 }
